af96427f4ba4b39e699fbe3050f2814914df1258
harunXh <husejnovic.harun@gmail.com>
2023-05-08 21:46:04 +0200

    feat(edittasks): add edit cancel and save

--- src/screens/ToDo/index.tsx
+++ src/screens/ToDo/index.tsx
@@ -1,36 +1,60 @@
 import { AntDesign } from '@expo/vector-icons';
 import { useState } from 'react';
 import { Pressable, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { Divider } from 'react-native-paper';
 
 import { Text, View } from '@/components/atoms/Themed';
 
 export default function TabThreeScreen() {
     type Task = {
         text: string;
+        isEditing: boolean;
     };
     //useState hook for array of tasks
-    const [tasks, setTasks] = useState<Task[]>([{ text: 'one' }, { text: 'two' }, { text: 'three' }]);
+    const [tasks, setTasks] = useState<Task[]>([
+        { text: 'one', isEditing: false },
+        { text: 'two', isEditing: false },
+        { text: 'three', isEditing: false },
+    ]);
     //useState hook for adding task
     const [newTask, setNewTask] = useState<string>('');
+    //
+    const [editedTask, setEditedTask] = useState<string>('');
+    const [editingIndex, setEditingIndex] = useState<number | null>(null);
 
     //function for adding task in the list
     const addTask = () => {
-        const newTasks = [...tasks, { text: newTask }];
+        const newTasks = [...tasks, { text: newTask, isEditing: false }];
         setTasks(newTasks);
         setNewTask('');
     };
 
     console.log('taskovi', tasks);
-    //function for clearin all tasks
+    //function for clearing all tasks
     const clearAll = () => {
         setTasks([]);
     };
 
-    //function for clearing signgle task on certiain index
+    //function for clearing single task on certiain index
     const clearSingleTask = (index: number) => {
         const newTasks = [...tasks];
         newTasks.splice(index, 1);
         setTasks(newTasks);
+    };
+
+    const handleStartEditing = (index: number) => {
+        setEditedTask(tasks[index].text);
+        setEditingIndex(index);
+    };
+
+    const handleSaveEditing = (index: number) => {
+        const newTasks = [...tasks];
+        newTasks[index].text = editedTask;
+        setTasks(newTasks);
+        setEditingIndex(null);
+    };
+    const handleCancelEditing = () => {
+        setEditingIndex(null);
     };
 
     return (
@@ -39,7 +63,7 @@ export default function TabThreeScreen() {
             <View>
                 <Text style={styles.title}>Add Tasks</Text>
                 <TextInput style={styles.taskInput} placeholder="Add task" value={newTask} onChangeText={setNewTask} />
-                <Pressable style={styles.pressableAdd} onPress={addTask} disabled={newTask === ''}>
+                <Pressable style={styles.pressableAdd} onPress={addTask} disabled={!newTask}>
                     <Text style={styles.addButton}>ADD</Text>
                 </Pressable>
             </View>
@@ -50,27 +74,46 @@ export default function TabThreeScreen() {
             <View>
                 {tasks.map((task, index) => (
                     <View style={styles.taskItem} key={index}>
-                        <Text style={styles.taskText}>{task.text}</Text>
-                        <View style={styles.taskItemButtons}>
-                            <TouchableOpacity onPress={() => clearSingleTask(index)}>
-                                <View style={styles.actionIcon}>
-                                    <Text style={styles.actionIconText}>DEL</Text>
+                        {editingIndex === index ? (
+                            <>
+                                <TextInput style={styles.taskText} value={editedTask} onChangeText={setEditedTask} />
+                                <View style={styles.taskItemButtons}>
+                                    <TouchableOpacity onPress={() => handleSaveEditing(index)}>
+                                        <View style={styles.actionIcon}>
+                                            <Text style={styles.actionIconText}>SAVE</Text>
+                                        </View>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity onPress={handleCancelEditing}>
+                                        <View style={styles.actionIcon}>
+                                            <Text style={styles.actionIconText}>CANCEL</Text>
+                                        </View>
+                                    </TouchableOpacity>
                                 </View>
-                            </TouchableOpacity>
-                            <TouchableOpacity>
-                                <View style={styles.actionIcon}>
-                                    <Text style={styles.actionIconText}>EDIT</Text>
+                            </>
+                        ) : (
+                            <>
+                                <Text style={styles.taskText}>{task.text}</Text>
+                                <Divider />
+                                <View style={styles.taskItemButtons}>
+                                    <TouchableOpacity onPress={() => clearSingleTask(index)}>
+                                        <View style={styles.actionIcon}>
+                                            <Text style={styles.actionIconText}>DEL</Text>
+                                        </View>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity onPress={() => handleStartEditing(index)}>
+                                        <View style={styles.actionIcon}>
+                                            <Text style={styles.actionIconText}>EDIT</Text>
+                                        </View>
+                                    </TouchableOpacity>
                                 </View>
-                            </TouchableOpacity>
-                        </View>
+                            </>
+                        )}
                     </View>
                 ))}
             </View>
             {/* BOTTOM */}
             <Pressable style={styles.clear} onPress={clearAll}>
-                <Text>
-                    <AntDesign name="delete" size={35} color="black" />
-                </Text>
+                <AntDesign name="delete" size={35} color="black" />
             </Pressable>
         </View>
     );
@@ -103,12 +146,11 @@ const styles = StyleSheet.create({
     },
     actionIcon: {
         flex: 1,
-        height: 24,
-        width: 40,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#ccfc',
-        marginLeft: 5,
+        marginHorizontal: 4,
+        paddingHorizontal: 8,
         borderRadius: 3,
         borderWidth: 1,
     },
@@ -130,11 +172,7 @@ const styles = StyleSheet.create({
         marginBottom: 20,
         textAlign: 'center',
     },
-    addTask: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginBottom: 20,
-    },
+
     taskItem: {
         flexDirection: 'row',
         justifyContent: 'space-between',
