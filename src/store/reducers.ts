539543da2b38d8d0f2f6c573a6c7803f52f795ff
harunh_ <korisnik@Korisniks-MacBook-Pro.local>
2023-05-25 18:08:59 +0200

    feat(store): add new store for projects

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -5,6 +5,7 @@ import score from './score/slice';
 import teamA from './teamA/slice';
 import teamB from './teamB/slice';
 import toDo from './toDo/slice';
+import projects from './toDoProjects/slice';
 
 const rootReducer = combineReducers({
     appSettings,
@@ -12,6 +13,7 @@ const rootReducer = combineReducers({
     teamB,
     score,
     toDo,
+    projects,
 });
 
 export default rootReducer;
--- src/store/toDoprojects/slice.ts
+++ src/store/toDoprojects/slice.ts
@@ -1,70 +1,32 @@
-// import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
 
-// import { Task as TaskType } from '@/constants/Types';
-
-// export type ToDoProjects = { tasks: TaskType[]; editingIndex: number | null; newText: string; name: string };
-
-// const initialState = {
-//     tasks: [
-//         { text: 'test1', done: false },
-//         { text: 'test2', done: false },
-//     ],
-//     editingIndex: null,
-//     newText: '',
-// } as ToDoProjects;
-
-// export const toDoSlice = createSlice({
-//     name: 'toDo',
-//     initialState,
-//     reducers: {
-//         addNewTask: (state, { payload }: PayloadAction<string>) => {
-//             state.tasks.push({ text: payload });
-//         },
-//         deleteTask: (state, { payload }: PayloadAction<number>) => {
-//             state.tasks.splice(payload, 1);
-//         },
-//         taskIsDone: (state, action: PayloadAction<number>) => {
-//             const index = action.payload;
-//             state.tasks[index].done = !state.tasks[index].done;
-//         },
-//         clearTasks: (state) => {
-//             state.tasks = [];
-//         },
-//         saveEditedTask: (state, { payload }: PayloadAction<string>) => {
-//             const index = state.editingIndex;
-//             const newText = payload;
-//             if (index !== null) {
-//                 state.tasks[index].text = newText;
-//                 state.editingIndex = null;
-//             }
-//         },
-//         toggleEditTask: (state, { payload }: PayloadAction<number>) => {
-//             const index = payload;
-//             state.editingIndex = index;
-//             state.newText = state.tasks[index].text;
-//         },
-//         cancelEditing: (state) => {
-//             state.editingIndex = null;
-//             state.newText = '';
-//         },
-//         setEditedText: (state, { payload }: PayloadAction<string>) => {
-//             state.newText = payload;
-//         },
-
-//         resetToDoState: () => initialState,
-//     },
-// });
-
-// export const {
-//     addNewTask,
-//     resetToDoState,
-//     deleteTask,
-//     taskIsDone,
-//     setEditedText,
-//     clearTasks,
-//     saveEditedTask,
-//     cancelEditing,
-//     toggleEditTask,
-// } = toDoSlice.actions;
-
-// export default toDoSlice.reducer;
+export type Project = {
+    id: string;
+    name: string;
+};
+const initialState: Project[] = [
+    {
+        id: uuidv4(),
+        name: 'Project 1',
+    },
+    {
+        id: uuidv4(),
+        name: 'Project 2',
+    },
+];
+const projectSlice = createSlice({
+    name: 'projects',
+    initialState,
+    reducers: {
+        addProject: (state, action: PayloadAction<string>) => {
+            state.push({ id: uuidv4(), name: action.payload });
+        },
+        deleteProject: (state, action: PayloadAction<string>) => {
+            return state.filter((project) => project.id !== action.payload);
+        },
+        resetProjectState: () => initialState,
+    },
+});
+export const { addProject, deleteProject, resetProjectState } = projectSlice.actions;
+export default projectSlice.reducer;
